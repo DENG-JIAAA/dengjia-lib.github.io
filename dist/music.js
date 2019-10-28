41a4692da7139b436ecbcd9a91dad295257e6c2f
dengjia-lib <2663239386@qq.com>
2019-10-28 16:55:17 +0800

    Site updated: 2019-10-28 16:55:12

--- dist/music.js
+++ dist/music.js
@@ -8,78 +8,79 @@ const ap = new APlayer({
         {
             name: '明年今日(Live)',
             artist: '陈奕迅',
-            url: '../songs/明年今日(Live).m4a',
-            cover: '../songs/明年今日(Live).jpg',
+            url: 'http://music.163.com/song/media/outer/url?id=64599.mp3',
+            cover: 'https://i.loli.net/2019/10/28/UCfPEeiTBj5GwJ3.jpg',
             lrc: '../songs/明年今日(Live).lrc'
         },
         {
             name: '最佳损友',
             artist: '陈奕迅',
-            url: '../songs/最佳损友.m4a',
-            cover: '../songs/最佳损友.jpg',
-            lrc: '../songs/最佳损友.lrc'
+            url: 'http://music.163.com/song/media/outer/url?id=65800.mp3',
+            cover: 'https://i.loli.net/2019/10/28/Ys6XaMtHo1lTLWe.jpg',
+            lrc: '
+'
         },
         {
             name: 'Nothing Ever Happened',
             artist: '陈奕迅',
-            url: '../songs/Nothing Ever Happened.m4a',
-            cover: '../songs/Nothing Ever Happened.jpg',
-            lrc: '../songs/Nothing Ever Happened.lrc'
+            url: 'http://music.163.com/song/media/outer/url?id=64682.mp3',
+            cover: 'https://i.loli.net/2019/10/28/IPvsCytcGUYfRNB.png',
+            lrc: 'songs/Nothing Ever Happened.lrc'
         },
         {
             name: '约定',
             artist: '王菲',
-            url: '../songs/约定.m4a',
-            cover: '../songs/约定.jpg',
-            lrc: '../songs/约定.lrc'
+            url: 'http://music.163.com/song/media/outer/url?id=299116.mp3',
+            cover: 'https://i.loli.net/2019/10/28/X9Dpv4FagUx25Ml.jpg',
+            lrc: 'songs/约定.lrc'
         },
         {
             name: '水星记',
             artist: '郭顶',
-            url: '../songs/水星记.m4a',
-            cover: '../songs/水星记.jpg',
-            lrc: '../songs/水星记.lrc'
+            url: 'http://music.163.com/song/media/outer/url?id=441491828.mp3',
+            cover: 'https://i.loli.net/2019/10/28/ipOA4LBR5EmVQFw.jpg',
+            lrc: 'songs/水星记.lrc'
         },
         {
             name: '在云端',
             artist: '郭顶',
-            url: '../songs/在云端.m4a',
-            cover: '../songs/在云端.jpg',
-            lrc: '../songs/在云端.lrc'
+            url: 'http://music.163.com/song/media/outer/url?id=442869201.mp3',
+            cover: 'https://i.loli.net/2019/10/28/ipOA4LBR5EmVQFw.jpg',
+            lrc: 'songs/在云端.lrc'
         },
         {
             name: '梦',
             artist: '柴田淳',
-            url: '../songs/梦.m4a',
-            cover: '../songs/梦.jpg',
+            url: 'http://music.163.com/song/media/outer/url?id=562732.mp3',
+            cover: 'https://i.loli.net/2019/10/28/6cOIKr4FGXy9Eio.jpg',
             lrc: ''
         },
         {
-            name: 'Try',
-            artist: 'AsherMonroe',
-            url: '../songs/AsherMonroe-Try.m4a',
-            cover: '../songs/AsherMonroe-Try.jpg',
+            name: 'Try电影《名扬四海》插曲',
+            artist: 'Asher Monroe',
+            url: 'http://music.163.com/song/media/outer/url?id=5041667.mp3',
+            cover: 'https://i.loli.net/2019/10/28/4eJNTiBkMrdwQOW.jpg',
             lrc: ''
         },
         {
             name: '早已离开我(Live)',
             artist: '张学友',
-            url: '../songs/早已离开我(Live).m4a',
-            cover: '../songs/早已离开我(Live).jpg',
+            url: 'http://music.163.com/song/media/outer/url?id=188857.mp3',
+            cover: 'https://i.loli.net/2019/10/28/QT42LzIrAuc83Ud.png',
             lrc: ''
         },
         {
             name: '光辉岁月',
             artist: 'Beyone',
-            url: '../songs/光辉岁月.m4a',
-            cover: '../songs/光辉岁月.jpg',
-            lrc: '../songs/光辉岁月.lrc'
+            url: 'http://music.163.com/song/media/outer/url?id=346576.mp3',
+            cover: 'songs/光辉岁月.jpg',
+            lrc: 'https://i.loli.net/2019/10/28/TN6dO18iYJeVv5C.jpg'
         },
         {
             name: 'The Other Side',
             artist: 'The Workday Release',
-            url: '../songs/The Other Side.m4a',
-            cover: '../songs/The Other Side.jpg',
+            url: 'http://music.163.com/song/media/outer/url?id=31090070.mp3',
+            cover: 'https://i.loli.net/2019/10/28/P3SlYNqpEdJKu6O.jpg',
             lrc: ''
         }
     ]
